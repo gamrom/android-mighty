8fb0861e34ea2607c1a367c05e177bf793042a77
gamrom <gamrom01@naver.com>
2020-04-10 10:16:13 +0900

    버튼 세개 만들어봄

---
+++ app/src/main/java/com/example/mighty/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.mighty
+
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    publci
+}
---
+++ app/src/main/java/com/example/mighty/stateChangeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mighty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class stateChangeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_state_change)
+    }
+}
